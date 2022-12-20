ea6fb47855265c1d85abf8e8f84489b43fe08589
IpekCansuINANC <72729857+IpekCansuINANC@users.noreply.github.com>
2022-12-20 16:05:21 +0300

    Initial commit

---
+++ src/components/row.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import axios from "../axios";
+import "./row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  useEffect(() => {
+    //if [], run once when the row loads, and dont run again.
+    const fetchData = async () => {
+      const response = await axios.get(fetchUrl);
+      console.log(response.data.results);
+      setMovies(response.data.results);
+    };
+    fetchData();
+  }, [fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1
+    }
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      // if a video is already open set empty
+      setTrailerUrl("");
+    } else {
+      //npm module promise
+      //getting the special url part
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            onClick={() => handleClick(movie)}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+};
+
+export default Row;
